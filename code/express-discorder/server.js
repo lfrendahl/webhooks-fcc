9a910eb09f13d991df52eedff519b25d979ba099
lfrendahl <lfrendahl3@gmail.com>
2023-07-25 15:28:58 -0400

    updated webhook to include sender name and repo name in post

--- code/express-discorder/server.js
+++ code/express-discorder/server.js
@@ -18,8 +18,10 @@ app.get("/", (req, res) => res.send(`
 `));
 
 app.post("/github", (req, res) => {
-  const content = ":wave: Hi mom!";
-  const avatarUrl = "https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif";
+  const sender = req.body.sender.login;
+  const repository = req.body.repository.name;
+  const content = `:taco: ${sender} justed starred ${repository}. Don't invite Becca to this server!`;
+  const avatarUrl = req.body.sender.avatar_url;
   axios
     .post(process.env.DISCORD_WEBHOOK_URL, {
       content: content,
